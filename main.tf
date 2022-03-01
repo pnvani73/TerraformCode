a4cd78e5e4e4a0115602da62634ad51338bb9322
pnvani73 <64314255+pnvani73@users.noreply.github.com>
2022-03-01 13:28:09 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,26 @@
+﻿terraform {
+  backend "azurerm" {
+
+    resource_group_name = "tfstate"
+    storage_account_name = "tfstate10571"
+    container_name = "tfstate"
+    key = "terraform.state"
+    }
+}
+provider "azurerm" {
+  version = "2.0.0"
+  features {}
+}
+resource "azurerm_storage_account" "TechTargetSG" {
+
+Name	= "tfstate10571"
+resource_group_name	= var.resourceGroupName
+Location	=	var.location
+account_tier	=	"Standard"
+
+account_replication_type = "GRS"
+
+tags = {
+
+environment = "development"
+}
