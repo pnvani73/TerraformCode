2d8ca8eebf96ba2e199c1f6f46e1327b812c6786
pnvani73 <64314255+pnvani73@users.noreply.github.com>
2022-03-01 13:28:30 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,15 @@
+﻿variable "resourceGroupName" {
+
+    type = string
+
+    description = "Resource group name for the storage account to reside in"
+    resourceGroupName = "dev2"
+}
+
+variable "location" {
+
+    type = string
+
+    description = "Region that the storage account will reside in"
+    location = "eastus"
+} 
